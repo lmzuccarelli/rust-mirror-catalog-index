719cc3675455780b1be331159ca1c186231ac2fb
Luigi Mario Zuccarelli <luzuccar@redhat.com>
2024-09-01 19:12:27 +0200

    Update log messaging

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 use custom_logger::*;
 use flate2::read::GzDecoder;
-use mirror_config::Operator;
-use mirror_copy::{get_blobs_file, FsLayer, ImageReference};
+use mirror_copy::FsLayer;
 use serde_derive::{Deserialize, Serialize};
 use std::collections::HashSet;
 use std::fs;
@@ -60,10 +59,13 @@ pub async fn untar_layers(
     for path in images.iter() {
         let blob = path.split(":").nth(1).unwrap();
         let cache_file = cache_dir.clone() + "/" + &blob[..6];
-        log.trace(&format!("cache file {}", cache_file.clone()));
+        log.trace(&format!(
+            "[untar_layers (lib)] cache file {}",
+            cache_file.clone()
+        ));
         if !Path::new(&cache_file).exists() {
-            let file = get_blobs_file(blobs_dir.clone(), blob);
-            log.trace(&format!("blobs file {}", file));
+            let file = format!("{}/{}/{}", blobs_dir.clone(), &blob[0..2], blob);
+            log.info(&format!("[untar_layers (lib)] blobs file {}", file));
             let tar_gz = File::open(file.clone()).expect("could not open file");
             let tar = GzDecoder::new(tar_gz);
             let mut archive = Archive::new(tar);
@@ -75,14 +77,21 @@ pub async fn untar_layers(
                 // we are really interested in either the configs or release-manifests directories
                 if path_str.contains("configs/") || path_str.contains("release-manifests/") {
                     // should always be a sha256 string
-                    log.info(&format!("untarring file {} ", &blob[..6]));
-                    let this_tar_gz = File::open(file.clone()).expect("could not open file");
+                    log.info(&format!(
+                        "[untar_layers (lib)] untarring file {} ",
+                        &blob[..6]
+                    ));
+                    let this_tar_gz =
+                        File::open(file.clone()).expect("[untar_layers] could not open blob file");
                     let this_tar = GzDecoder::new(this_tar_gz);
                     let mut this_archive = Archive::new(this_tar);
                     match this_archive.unpack(cache_file.clone()) {
                         Ok(arch) => arch,
                         Err(error) => {
-                            let msg = format!("skipping this error : {} ", &error.to_string());
+                            let msg = format!(
+                                "[untar_layers (lib)] skipping this error : {} ",
+                                &error.to_string()
+                            );
                             log.warn(&msg);
                         }
                     };
@@ -90,49 +99,9 @@ pub async fn untar_layers(
                 }
             }
         } else {
-            log.info(&format!("cache exists {}", cache_file));
+            log.info(&format!("[untar_layers (lib)] cache exists {}", cache_file));
         }
     }
-}
-
-// parse_image_index - best attempt to parse image index and return catalog reference
-pub fn parse_image_index(log: &Logging, operators: Vec<Operator>) -> Vec<ImageReference> {
-    let mut image_refs = vec![];
-    for ops in operators.iter() {
-        let img = ops.catalog.clone();
-        log.trace(&format!("catalogs {:#?}", img));
-        let mut hld = img.split("/");
-        let reg = hld.nth(0).unwrap();
-        let ns = hld.nth(0).unwrap();
-        let index = hld.nth(0).unwrap();
-        let mut i = index.split(":");
-        let name = i.nth(0).unwrap();
-        let ver = i.nth(0).unwrap();
-        let ir = ImageReference {
-            registry: reg.to_string(),
-            namespace: ns.to_string(),
-            name: name.to_string(),
-            version: ver.to_string(),
-        };
-        log.debug(&format!("image reference {:#?}", img));
-        image_refs.insert(0, ir);
-    }
-    image_refs
-}
-
-// get_cache_dir
-pub fn get_cache_dir(dir: String, name: String, version: String, arch: Option<String>) -> String {
-    let mut file = dir.clone();
-    file.push_str(&name);
-    file.push_str(&"/");
-    file.push_str(&version);
-    file.push_str(&"/");
-    if arch.is_some() {
-        file.push_str(&arch.unwrap());
-        file.push_str(&"/");
-    }
-    file.push_str(&"cache");
-    file
 }
 
 // find a specific directory in the untar layers
@@ -144,12 +113,13 @@ pub async fn find_dir(log: &Logging, dir: String, name: String) -> String {
     match paths {
         Ok(res_paths) => {
             for path in res_paths {
-                let entry = path.expect("could not resolve path entry");
+                let entry = path.expect("[find_dir (lib)] could not resolve path entry");
                 let file = entry.path();
                 // go down one more level
                 let sub_paths = fs::read_dir(file).unwrap();
                 for sub_path in sub_paths {
-                    let sub_entry = sub_path.expect("could not resolve sub path entry");
+                    let sub_entry =
+                        sub_path.expect("[find dir (lib)] could not resolve sub path entry");
                     let sub_name = sub_entry.path();
                     let str_dir = sub_name.into_os_string().into_string().unwrap();
                     if str_dir.contains(&name) {
@@ -159,8 +129,12 @@ pub async fn find_dir(log: &Logging, dir: String, name: String) -> String {
             }
         }
         Err(error) => {
-            let msg = format!("{} ", error);
-            log.warn(&msg);
+            let msg = format!(
+                "[find_dir (lib)] dir: {} {}",
+                dir.clone(),
+                error.to_string().to_lowercase()
+            );
+            log.error(&msg);
         }
     }
     return "".to_string();
@@ -173,6 +147,7 @@ pub fn parse_json_manifest(data: String) -> Result<ManifestSchema, Box<dyn std::
     Ok(root)
 }
 
+/*
 // contruct the manifest url
 pub fn get_image_manifest_url(image_ref: ImageReference) -> String {
     // return a string in the form of (example below)
@@ -208,11 +183,12 @@ pub fn get_manifest_json_file(
     file.push_str(&"manifest.json");
     file
 }
+*/
 
 #[cfg(test)]
 mod tests {
 
-    use mirror_config::Operator;
+    //use mirror_config::Operator;
     use std::fs;
 
     // this brings everything from parent's scope into this scope
@@ -224,6 +200,7 @@ mod tests {
         };
     }
 
+    /*
     #[test]
     fn get_cache_dir_pass() {
         let res = get_cache_dir(
@@ -249,6 +226,7 @@ mod tests {
         assert_eq!(res.len(), 1);
         assert_eq!(res[0].registry, String::from("test.registry.io"));
     }
+    */
 
     #[test]
     fn untar_layers_pass() {
@@ -260,6 +238,7 @@ mod tests {
             blob_sum: String::from("sha256:ac202b"),
             original_ref: Some(String::from("test-ac")),
             size: Some(112),
+            number: None,
         };
         vec_layers.insert(0, fslayer);
         aw!(untar_layers(
@@ -294,6 +273,7 @@ mod tests {
         assert!(res.is_ok());
     }
 
+    /*
     #[test]
     fn get_image_manifest_url_pass() {
         let imageref = ImageReference {
@@ -320,4 +300,5 @@ mod tests {
             String::from("./test-artifacts/index-manifest/v1/manifest.json")
         );
     }
+    */
 }
